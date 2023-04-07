84168ca5d66024d2be09832edf98b19cff5068c3
Pavel <pmaskarev@vk.com>
2023-04-07 09:46:03 +0300

    Added Task034

---
+++ Task034/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+
+System.Console.WriteLine("Введите размер массива:");
+int Len = int.Parse(Console.ReadLine());
+int [] CreateMas(int Len)
+{
+    int[] Mas = new int[Len];
+    for (int i = 0; i < Mas.Length; i++)
+    {
+        Mas[i] = new Random().Next(100, 1000);
+    }
+    return Mas;
+}
+
+int[] Mas = CreateMas(Len);
+
+void PrintMas(int[] Mas)
+{
+    int Kol = 0;
+    System.Console.Write("[");
+    for (int i = 0; i < Mas.Length-1; i++)
+    {
+        System.Console.Write(Mas[i]+"; ");
+    }
+    System.Console.WriteLine(Mas[Mas.Length-1]+"]");
+    for (int i = 0; i < Mas.Length; i++)
+    {
+        if (Mas[i] % 2 == 0)
+        {
+            Kol++;
+        }
+    }
+    System.Console.WriteLine($"Количество четных чисел в массиве: {Kol}");
+}
+
+PrintMas(Mas);
+
